86c079e78c698814498ea2fc80fce28b50afdbfe
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2025-04-04 10:50:26 -0400

    Create p2.c

---
+++ test/p2.c
@@ -0,0 +1,20 @@
+//symbolic constant to set a max size for the string
+#define SIZE 1000
+
+//function prototype called text_statistics that calculates statistics on a passed string
+void text_statistics(const char *str);
+
+int main(void) {
+
+  char str[SIZE];
+  
+  // getting a string using the fgets function
+  puts("Enter a string: ");
+  fgets(str, SIZE - 1, stdin);
+  puts("");
+
+  //function call
+  text_statistics(str);
+  
+  return 0;
+}
